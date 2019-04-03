889124e9bdb0de1cd6c3aeee1873068e4568f46a
Jon Dunnett <dunnett_jonathan@wheatoncollege.edu>
2019-04-03 14:38:59 -0700

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -30,17 +30,13 @@ void read_grammar(char * infile, Grammar &G) {
   fscanf(ifp,"l:%d\n",&l);
   fscanf(ifp,"w:%d\n",&w);
   fscanf(ifp,"%c\n",&c);
-  printf("%d",1);
   if (c=='{') {
-    printf("%d",2);
     while (!done){
-      printf("%d",3);
+      prod="";
       fscanf(ifp,"%c",&c);
       if (c=='}') {
-        printf("%d",5);
         done+=1;
       } else {
-        printf("%d",4);
         fscanf(ifp,":%s\n", prod.data());
         G[c]=prod;
       }
