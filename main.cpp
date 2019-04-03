d97215640110aade8ad234b018d66c00cf02e735
Jon Dunnett <dunnett_jonathan@wheatoncollege.edu>
2019-04-03 02:56:44 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,12 @@
 int main (int argc, char** argv) {
   Grammar G;
   char y,*x ,* p = (char *) malloc(sizeof(char)*10);
+  x=p;
+  *x++='p';
+  *x='\0';
+
+  G['C']=p;
+
 
   printf("input: %s\n",argv[1]);
   printf("input: %s\n",argv[2]);
